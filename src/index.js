1a029362c8c3211ed24cbc27e6fe02ff080de480
Daniel Johnson <djohnson@squarespace.com>
2016-06-16 14:17:47 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const interpolate = require('./interpolate');
+
+try {
+  module.exports = interpolate;
+} catch (err) {
+  console.error(err);
+}
---
+++ src/interpolate.js
@@ -0,0 +1,47 @@
+/**
+ * Interpolate the value within a target range based on a known value
+ * within a refernce range.
+ *
+ * @param {Array} an array of two numbers.
+ * @param {Array} an array of two numbers.
+ * @param {Number} known value within referenceRange.
+ * @return {Number} vlaue within targetRange.
+ */
+
+const interpolate = (referenceRange, targetRange, referenceValue) => {
+  if (!Array.isArray(referenceRange) || !Array.isArray(targetRange) || typeof referenceValue !== 'number' || referenceRange.length !== 2 || targetRange.length !== 2) {
+    return;
+  }
+
+  const rStart = referenceRange[0];
+  const rEnd = referenceRange[1];
+  const rLower = Math.min(rStart, rEnd);
+  const rUpper = Math.max(rStart, rEnd);
+  const tStart = targetRange[0];
+  const tEnd = targetRange[1];
+  const tUpper = Math.max(tStart, tEnd);
+
+  if (referenceValue > rUpper || referenceValue < rLower) {
+    console.warn(referenceValue + ' is not between ' + rLower + ' and ' + rUpper);
+    return;
+  }
+
+  const rDiff = Math.abs(rStart - rEnd);
+  const tDiff = Math.abs(tStart - tEnd);
+
+  const mod = (() => {
+    if (rStart === rUpper) {
+      return tStart === tUpper ? tEnd : 0 - tEnd;
+    } else {
+      return tStart === tUpper ? 0 - tStart : tStart;
+    }
+  })();
+
+  return Math.abs((referenceValue - rLower) / rDiff * tDiff + mod);
+};
+
+try {
+  module.exports = interpolate;
+} catch (err) {
+  console.error(err);
+}
